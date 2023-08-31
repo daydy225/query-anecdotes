125b65b160f0570bcbde8b6d5c140a85377da5dd
daydy225 <dabelydadi@gmail.com>
2023-08-31 16:54:42 +0000

    Implement voting for anecdotes using React Query

--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 <p>Implement adding new anecdotes to the server using React Query</p>
 <p>The application should render a new anecdote by default. Note that the content of the anecdote must be at least 5 characters long, otherwise the server will reject the POST request</p>
 <p>You don't have to worry about error handling now</p>
+
+## Exercises 6.22
+<p>Implement voting for anecdotes using again the React Query</p>
+<p>The application should automatically render the increased number of votes for the voted anecdote</p>
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { fetchAnectodes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { fetchAnectodes, updateAnecdoteVote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
   const { isLoading, isError, data } = useQuery({
     queryKey: ['anecdotes'],
     queryFn: fetchAnectodes,
     retry: 1,
   })
 
-  const handleVote = anecdote => {
-    console.log('vote')
-  }
+  const updateAnecdoteVoteMutation = useMutation(updateAnecdoteVote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    },
+  })
 
-  console.log('anecdotes data', data)
+  const handleVote = anecdote => {
+    updateAnecdoteVoteMutation.mutate({
+      ...anecdote,
+      votes: anecdote.votes + 1,
+    })
+  }
 
   const anecdotes = data
 
